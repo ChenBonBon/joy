409691612907b658d9c104e27d79faa9b50c1c00
bonbon <per_cherry@163.com>
2022-07-20 23:52:01 +0800

    add Button

---
+++ packages/button/index.tsx
@@ -0,0 +1,5 @@
+import { Button } from "antd";
+
+export default function JoyButton() {
+  return <Button type="primary">Click</Button>;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "packages/index.ts"),
+      name: "joy",
+      fileName: (format) => `joy.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "antd"],
+      output: {
+        globals: {
+          react: "react",
+          antd: "antd",
+          "react-dom": "react-dom",
+        },
+      },
+    },
+  },
+});
