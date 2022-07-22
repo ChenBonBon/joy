188f77fb2a56ca55d9ed981a090ede8a2eced159
BonBon <bonbon.chen@basebit.ai>
2022-07-22 10:14:01 +0800

    新增slider

--- packages/index.ts
+++ packages/index.ts
@@ -3,3 +3,4 @@ import "antd/dist/antd.variable.css";
 export { default as Button } from "./button";
 export type { JoyButtonProps } from "./button";
 export { default as ConfigProvider } from "./config-provider";
+export { default as Slider } from "./slider";
